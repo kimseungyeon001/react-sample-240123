ac32f7c49d212a174844a4f7745f90303acb6645
kimseungyeon001 <kim.seungyeon@classmethod.jp>
2024-01-29 12:05:41 +0900

    improvement mock init

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -1,9 +1,4 @@
 export const initMocks = async (): Promise<void> => {
-  if (typeof window === 'undefined') {
-    const { server } = await import('./server')
-    server.listen({ onUnhandledRequest: 'bypass' })
-  } else {
-    const { worker } = await import('./browser')
-    await worker.start({ onUnhandledRequest: 'bypass' })
-  }
+  const { worker } = await import('./browser')
+  await worker.start({ onUnhandledRequest: 'bypass' })
 }
